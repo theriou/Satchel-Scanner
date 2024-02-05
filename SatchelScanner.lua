26c7d54caac6a5eebaa13910880a4f0ae4a2e423
theriou <theriou@users.noreply.github.com>
2024-02-04 21:42:11 -0600

    Update to Patch 10.2.5

--- SatchelScanner.lua
+++ SatchelScanner.lua
@@ -26,7 +26,7 @@ SS_CloseButton = "Interface\\Addons\\SatchelScanner\\textures\\close.tga";
 
 -- Variables
 local running = false;
-SS_addonVersion = 10.20;
+SS_addonVersion = 1025.0;
 SS_versionTag = "Release";
 SS_TimeSinceLastNotification = 0;
 
