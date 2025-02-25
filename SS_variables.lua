65e71e3965950e263b14867eda7444157bd15f20
theriou <theriou@users.noreply.github.com>
2025-02-25 14:23:36 -0600

    Update to WoW 11.1.0

--- SS_variables.lua
+++ SS_variables.lua
@@ -2,7 +2,7 @@ SS_savedVariables = {
 	-- {"savedvar", defaultValue, "valueinherit"}
 	SS_ScannerInterval = {"scantimer", 5, "SS_ScannerIntervalSlider"},
 	SS_NotificationInterval = {"notificationtimer", 10, "SS_NotificationIntervalSlider"},
-	SS_addonVersion = {"version", 1107.0},
+	SS_addonVersion = {"version", 1110.0},
 	SS_satchelsReceived = {"satchels", 0},
 	SS_showUI = {"showMainFrame", true},
 	-- Buttons
